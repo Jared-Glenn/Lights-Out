48634e9b65bc0114903d22f38eddf8255a91fb72
Jared <jared_glenn@yahoo.com>
2024-03-03 16:43:16 -0700

    add working components

--- src/Board.jsx
+++ src/Board.jsx
@@ -27,7 +27,7 @@ import "./Board.css";
  *
  **/
 
-function Board({ nrows, ncols, chanceLightStartsOn }) {
+function Board({ nrows=3, ncols=3, chanceLightStartsOn=50 }) {
   const [board, setBoard] = useState(createBoard(nrows, ncols, chanceLightStartsOn));
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
@@ -51,34 +51,26 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
 
   function hasWon() {
     // TODO: check the board in state to determine whether the player has won.
-    let containsTrue = board.some(row => row.some(node => node === true));
+    let containsFalse = board.some(row => row.some(node => node === false));
 
-    return !containsTrue;
+    return !containsFalse;
   }
 
   function flipCellsAround(coord) {
     console.log("RUNNING!")
     setBoard(oldBoard => {
       const [y, x] = coord.split("-").map(Number);
-      const {ncols, nrows} = board;
-      console.log(y, x);
-      console.log(ncols);
-      console.log(nrows);
 
       const flipCell = (y, x, boardCopy) => {
         // if this coord is actually on board, flip it
 
         if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
-          console.log("IN IF STATEMENT!")
           boardCopy[y][x] = !boardCopy[y][x];
         }
       };
 
     // TODO: Make a (deep) copy of the oldBoard
     const boardCopy = oldBoard.map(column => column.slice());
-    console.log(boardCopy);
-    console.log(oldBoard);
-    console.log(boardCopy === oldBoard);
     // TODO: in the copy, flip this cell and the cells around it
 
     flipCell(y, x, boardCopy);
